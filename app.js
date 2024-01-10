14e544f89e1b138a02730df49766861c236614e1
Mac <sophanpa@gmail.com>
2024-01-11 00:31:50 +0100

    Edit

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
 button = document.querySelector('#cookie_button');
-counter = 1;
+
+counter = 2;
+let jumpSound = new Audio('sounds/jump.mp3');
 button.addEventListener('click', () => {
+  jumpSound.volume = 0.2;
+  jumpSound.play();
+  document.querySelector('img').classList.add('jump');
   console.log('lol');
   document.querySelector('#counter').innerHTML = counter++;
+  setTimeout(() => {
+    document.querySelector('img').classList.remove('jump');
+  }, 200);
 });
 
 console.log(counter);
