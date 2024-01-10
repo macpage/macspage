8a5c732066ff2b6291774b144bb859fa84556450
Mac <sophanpa@gmail.com>
2024-01-10 23:50:51 +0100

    Edit

---
+++ app.js
@@ -0,0 +1,5 @@
+button = document.querySelector('#cookie_button');
+button.addEventListener('click', console.log('BRTOOO'));
+
+document.querySelector('#counter').innerHTML = 3;
+console.log(counter);
